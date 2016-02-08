5e59252b2b44a6b065bd96aead3af33fa31e4600
Gelio <voreny.gelio@gmail.com>
2016-02-08 12:54:44 +0100

    added nodes and links, needs fixing

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,10 +1,6 @@
 var config = {
     width: 900,
     height: 600,
-    size: {
-        x: 1,
-        y: 1
-    },
     dataURL: 'http://www.freecodecamp.com/news/hot'
 };
 
--- app/scripts/force-graph.js
+++ app/scripts/force-graph.js
@@ -14,17 +14,56 @@ function prepareGraph() {
 
     this.forceLayout = d3.layout.force()
         .size([
-            config.size.x,
-            config.size.y
+            config.width,
+            config.height
         ]);
 }
 
 function drawData(data) {
-    var processed = processData(data);
+    var processed = processData(data),
+        nodes = [],
+        links = [];
 
-    processed.domains.forEach((function(domain) {
+    processed.domains.forEach(function(domain, index) {
+        nodes.push({
+            index: index
+        });
+    });
 
-    }).bind(this));
+    processed.users.forEach(function(user, index) {
+        nodes.push({
+            index: index
+        })
+    });
 
-    // Add nodes and links
+    processed.links.forEach(function(link) {
+        links.push({
+            source: link.domain,
+            target: processed.domains.length + link.user
+        });
+    });
+
+    this.forceLayout
+        .nodes(nodes)
+        .links(links)
+        .start();
+
+    var node = this.chart.selectAll('.node')
+        .data(nodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; })
+        .attr('r', function(d) { return d.weight; });
+
+    var link = this.chart.selectAll('.link')
+        .data(links)
+        .enter()
+        .append('line')
+        .attr('class', 'link')
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 }
